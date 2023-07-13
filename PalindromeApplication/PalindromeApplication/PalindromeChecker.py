f35b1ed222c37d1a65ee92f727582d5d36708607
DavidBrittain1 <davidbrit@live.com>
2023-07-13 13:57:58 +0100

    Provided examples and a working user input
    Had to change the startup file

---
+++ PalindromeApplication/PalindromeApplication/PalindromeChecker.py
@@ -0,0 +1,36 @@
+
+
+userInput = str(input("Please enter in a word or sentence to check if it is a Palindrome. "))
+
+
+
+def ValidatePalindrome(Palindrome): 
+   
+  # Step 1: Combine the string into one word
+  thislist = Palindrome.split()
+  newlist = ""
+  for word in thislist:
+    newlist = newlist + word
+
+    # Step 2: Convert the string to all lowercase
+  lowerPalindrome = newlist.lower()
+
+    # Step 3: Reverse the string
+  reverseLower = lowerPalindrome [::-1]
+  if reverseLower == lowerPalindrome:
+          
+    print("Yes, it is a Palindrome")
+    
+  else:
+    
+    print("No, it is not a Palindrome")
+
+ValidatePalindrome(userInput)
+
+
+
+              
+
+
+
+
--- readme.txt
+++ readme.txt
@@ -1,1 +1,13 @@
 Enter in a sentence or word, the program will analyse and tell you if it is a palindrome.
+
+This task was from Google Collab at: 
+https://colab.research.google.com/drive/1vTm6eeADq84RmXz19ZgRGhK3LaQDKdkw#scrollTo=qGhQm5tVEqU7
+
+
+EXAMPLES TO TEST:
+
+"Go hang a salami I'm a lasagna hog.", "Was it a rat I saw?", "Step on no pets", 
+"Sit on a potato pan, Otis", "Lisa Bonet ate no basil",
+"Satan, oscillate my metallic sonatas", "I roamed under it as a tired nude Maori", 
+"Rise to vote sir", or the exclamation "Dammit, I'm mad!". 
+Note that punctuation, capitalization, and spacing are usually ignored.
