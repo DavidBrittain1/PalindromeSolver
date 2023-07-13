934d5d97bd6fea537d9c43b503e2d3d2014bdcf2
DavidBrittain1 <davidbrit@live.com>
2023-07-13 13:40:13 +0100

    Created an Application in Visual Studio to launch changes

--- PalindromeChecker.py
+++ PalindromeChecker.py
@@ -9,35 +9,22 @@ def ValidatePalindrome(Palindrome):
 
     # Step 2: Convert the string to all lowercase
   lowerPalindrome = newlist.lower()
-  print(lowerPalindrome)
 
     # Step 3: Reverse the string
   reverseLower = lowerPalindrome [::-1]
-  print(reverseLower)
   if reverseLower == lowerPalindrome:
           
-    return True
+    print("Yes, it is a Palindrome")
     
   else:
     
-    return False
+    print("No, it is not a Palindrome")
 
-  
+  userInput = str(input("Please enter in a word or sentence to check if it is a Palindrome. "))
+  ValidatePalindrome(userInput)
 
-
-if ValidatePalindrome("Rise to vote sir"):
-  print("yes")
-else:
-  print("no")                   
+              
 
 
 
-#TestString = "Does this strip?"
-#SplitString = TestString.split()
-#StrippedString = ""
-#x = 0
-#while x < len(SplitString):
-#	x += 1
-#	for y in SplitString:
-#		StrippedString = StrippedString.join(y)
-#print(StrippedString)
+
